1325731ed57ddd3d3414138ce6bfb63ad165dfef
itshiroto <rivowowor7@gmail.com>
2022-04-27 21:52:26 +0800

    New branch for learning react hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,58 @@
-import { Flex } from "@chakra-ui/react";
-import Header from "./components/Header";
-import Body from "./components/Body";
+import { useState, useEffect, createContext, useContext, useReducer } from 'react'
+import { Box, Flex, Button, Text, Heading } from '@chakra-ui/react'
+
+const UserContext = createContext();
+
+function UserName() {
+  const user = useContext(UserContext);
+  return <Heading>Hello, {user}!</Heading>
+}
+
+function UserBase() {
+  return (
+    <UserName />
+  )
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      return { val: state.val + action.payload };
+    case 'decrement':
+      return { val: state.val - action.payload };
+    case 'reset':
+      return { val: 0 };
+  }
+}
 
 function App() {
+  const [user, setUser] = useState("Rivo");
+
+  const toggleUser = () => {
+    user === "Rivo" ? setUser("Agus") : setUser("Rivo");
+  }
+
+  useEffect(() => {
+    document.title = `Hello, ${user}!`;
+  });
+
+  const [counter, dispatch] = useReducer(reducer, { val: 0 });
 
   return (
-    <Flex direction='column' minH="100vh">
-      <Header />
-      <Body />
-    </Flex>
-
+    <UserContext.Provider value={user}>
+      <Flex h="100vh" justify="center" align="center">
+        <Flex justify="center" direction="column">
+          <UserBase />
+          <Button onClick={toggleUser}>Ganti Nama</Button>
+          <Text textAlign="center" mt="5" fontSize="xl">{counter.val}</Text>
+          <Flex gap="3">
+            <Button flexGrow={1} onClick={() => dispatch({ type: 'increment', payload: 1 })}>+</Button>
+            <Button flexGrow={1} onClick={() => dispatch({ type: 'decrement', payload: 1 })}>-</Button>
+            <Button flexGrow={1} onClick={() => dispatch({ type: 'reset' })}>Reset</Button>
+          </Flex>
+        </Flex>
+      </Flex>
+    </UserContext.Provider>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { ThemeProvider } from '@emotion/react'
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: 'dark',
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <ThemeProvider theme={theme}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ThemeProvider>
   </React.StrictMode>
 )
